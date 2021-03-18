4e4944476ba8b1621454306b131866fd7aa26b6b
Niklas Lantau <niklaslantau@gmail.com>
2021-03-18 15:18:29 +0100

    update

--- .config/scripts/xinput_config.sh
+++ .config/scripts/xinput_config.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# nlantau, 2021-03-18
+
+
 # Get Touchpad ID
-ID=$(xinput list | grep Touchpad | cut -f 2 | tr -d id=)
+IDTOUCH=$(xinput list | grep Touchpad | cut -f 2 | grep -Eo '[0-9]{1,2}')
+SCROLLPROP=$(xinput list-props 13 | grep -E 'Natural Scrolling Enabled \('| grep -Eo '[0-9]{2,3}')
+TAPPROP=$(xinput list-props 13 | grep -E 'Tapping Enabled \('| grep -Eo '[0-9]{2,3}')
+
 
 # Enable natural scrolling
-xinput set-prop $ID 314 1
+xinput set-prop $IDTOUCH $SCROLLPROP 1
 
 # Enable tap
-xinput set-prop $ID 332 1
+xinput set-prop $IDTOUCH $TAPPROP 1
 
 # TODO: G603

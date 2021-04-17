e49b4245eb33f3b603ad71c615f4f8edcf28f09a
Niklas Lantau <niklaslantau@gmail.com>
2021-04-18 01:56:21 +0200

    update

---
+++ .config/scripts/run_intellij.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+/home/nlantau/IntelliJ/idea-IU-211.6693.111/bin/idea.sh
+
+
+
+
+
--- .config/scripts/test.md
+++
@@ -1,9 +0,0 @@
-# Test
-
-hej hopp
-1. asd
-2. 123
-
-```python
-import pty; pty.spawn('/bin/bash')
-```
